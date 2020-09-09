46badce83720d7f9b28c21b9944bb9cc3ca3d8b2
Mike Smith <gooseisdead@mac.com>
2020-09-09 21:26:20 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,6 +1,7 @@
 
 def run_guessing_game
-    number = rand(1..6).to_s
+    number = rand(1..6)
+    number = number.to_s
     guess = gets.chomp
     if guess == num
         puts "You guessed the correct number!"
@@ -10,14 +11,14 @@ def run_guessing_game
         puts "Sorry! The computer guessed #{num}."
     end
 end
-def run_guessing_game
-  number = rand(1..6).to_s
-  input = gets.chomp
-  if input == number
-    puts "You guessed the correct number!"
-  elsif input == "exit"
-    puts "Goodbye!"
-  else
-    puts "The computer guessed #{number}."
-  end
-end
+# def run_guessing_game
+#   number = rand(1..6).to_s
+#   input = gets.chomp
+#   if input == number
+#     puts "You guessed the correct number!"
+#   elsif input == "exit"
+#     puts "Goodbye!"
+#   else
+#     puts "The computer guessed #{number}."
+#   end
+# end
